7fc1775c6f4ef535b1a9dcefaa8bde1f92a71753
Theodore Bisson <ruxpin32@users.noreply.github.com>
2016-01-24 11:13:40 -0800

    Initial Commit

---
+++ MainForm.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace TCBinaryViewer
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.c_text = new System.Windows.Forms.RichTextBox();
+			this.SuspendLayout();
+			// 
+			// c_text
+			// 
+			this.c_text.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.c_text.BackColor = System.Drawing.Color.Black;
+			this.c_text.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.c_text.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.c_text.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+			this.c_text.Location = new System.Drawing.Point(12, 12);
+			this.c_text.Name = "c_text";
+			this.c_text.ReadOnly = true;
+			this.c_text.Size = new System.Drawing.Size(814, 856);
+			this.c_text.TabIndex = 0;
+			this.c_text.Text = "";
+			// 
+			// MainForm
+			// 
+			this.AllowDrop = true;
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.Black;
+			this.ClientSize = new System.Drawing.Size(838, 880);
+			this.Controls.Add(this.c_text);
+			this.Name = "MainForm";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "TC Binary Viewer";
+			this.DragDrop += new System.Windows.Forms.DragEventHandler(this.MainForm_DragDrop);
+			this.DragEnter += new System.Windows.Forms.DragEventHandler(this.MainForm_DragEnter);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.RichTextBox c_text;
+	}
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TCBinaryViewer
+{
+	public partial class MainForm : Form
+	{
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		public MainForm()
+		{
+			InitializeComponent();
+
+			this.Icon = Icon.FromHandle(Properties.Resources.icon.GetHicon());
+
+			// Try to load a file from the command line.
+			string[] args = Environment.GetCommandLineArgs();
+			if (args.Length > 1)
+			{
+				SetFile(args[1]);
+			}
+			else
+			{
+				c_text.Text = "Drop a file onto this window to view its contents.";
+			}
+		}
+
+		/// <summary>
+		/// Drag and Drop filter, allows only file drops.
+		/// </summary>
+		private void MainForm_DragEnter(object sender, DragEventArgs e)
+		{
+			if (e.Data.GetDataPresent(DataFormats.FileDrop))
+				e.Effect = DragDropEffects.Copy;
+		}
+
+		/// <summary>
+		/// Drag and drop filter, processes file drops.
+		/// </summary>
+		private void MainForm_DragDrop(object sender, DragEventArgs e)
+		{
+			string[] files = e.Data.GetData(DataFormats.FileDrop) as string[];
+			if (files != null && files.Length > 0)
+			{
+				SetFile(files[0]);
+			}
+		}
+
+		/// <summary>
+		/// Reads the contents of the file specified, and if successful passes the data to the display method.
+		/// </summary>
+		/// <param name="path"></param>
+		public void SetFile(string path)
+		{
+			// Read all data in the file.
+			try
+			{
+				byte[] data = File.ReadAllBytes(path);
+				if (data != null)
+				{
+					this.Text = "TC Binary Viewer - " + path;
+					DisplayBytes(data);
+					return;
+				}
+			}
+			catch (Exception)
+			{
+				MessageBox.Show("Unable to load the contents of the file!", "Error Loading", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				c_text.Text = "Drop a file onto this window to view its contents.";
+			}
+
+			this.Text = "TC Binary Viewer";
+			c_text.Text = "Drop a file onto this window to view its contents.";
+		}
+
+		/// <summary>
+		/// Displays the actual bytes in the panel.
+		/// </summary>
+		private void DisplayBytes(byte[] data)
+		{
+			// Digits used for display.
+			char[] digits = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+
+			// Create an appropriately large data buffer.
+			char[] buffer = new char[(84 * (data.Length / 16)) + 100];
+
+			// Dump all data to the string.
+			int src = 0;
+			int dst = 0;
+			while (src < data.Length)
+			{
+				// Take this one row at a time.
+				int rowOffset = src;
+				int rowLength = data.Length - rowOffset;
+
+				// Row address.
+				UInt32 address = (UInt32) rowOffset;
+				UInt32 mask    = 0xF0000000;
+				int    shift   = 28;
+				for (int i = 0; i < 8; ++i)
+				{
+					int value = (int) (address & mask) >> shift;
+					buffer[dst++] = digits[value];
+					mask >>= 4;
+					shift -= 4;
+				}
+
+				// Row bytes.
+				buffer[dst++] = ' ';
+				buffer[dst++] = ' ';
+				for (int i = 0; i < rowLength && i < 16; ++i)
+				{
+					Byte value = data[src++];
+					buffer[dst++] = digits[(value & 0xF0) >> 4];
+					buffer[dst++] = digits[(value & 0x0F)];
+
+					if (i == 7)
+						buffer[dst++] = '-';
+					else
+						buffer[dst++] = ' ';
+				}
+				src = rowOffset;
+
+				// Row ASCII.
+				buffer[dst++] = ' ';
+				buffer[dst++] = ' ';
+				buffer[dst++] = ' ';
+				for (int i = 0; i < rowLength && i < 16; ++i)
+				{
+					Byte value = data[src++];
+					if (value >= ' ' && value <= '~')
+						buffer[dst++] = (char) value;
+					else
+						buffer[dst++] = '.';
+				}
+
+				// End of line.
+				buffer[dst++] = '\r';
+				buffer[dst++] = '\n';
+
+				src += 16;
+			}
+			buffer[dst] = '\0';
+
+			// Convert to a string and display.
+			c_text.Text = new string(buffer);
+		}
+	}
+}
